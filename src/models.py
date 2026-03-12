6560c9ee686885d4ec705858706e92e7bbaeb8d4
V Majarocon <56297035+Veerific@users.noreply.github.com>
2026-03-12 19:57:40 +0100

    Finished training models

--- src/models.py
+++ src/models.py
@@ -26,19 +26,19 @@ class SimpleCNN(nn.Module):
         super().__init__()
         self.features = nn.Sequential(
             nn.Conv2d(3, 6, kernel_size=5, padding=0, stride=1), # Layer 1: Convolution (1 32x32 => 6 28x28)
-            nn.ReLU(inplace=True),
+            nn.ReLU(),
             nn.MaxPool2d(2, 2), # Layer 2: Pooling (6 28x28 => 6 14x14)
 
             nn.Conv2d(6, 16, kernel_size=5, padding=0), # Layer 3: Convolution (6 14x14 => 16 10x10)
-            nn.ReLU(inplace=True),
+            nn.ReLU(),
             nn.MaxPool2d(2, 2), # Layer 4: Pooling (16 10x10 => 16 5x5)
             
         )
         self.classifier = nn.Sequential(
             nn.Linear(400, 120), # Layer 5: Fully Connects the flattened layer (16 5x5 => 1 120)
-            nn.ReLU(inplace=True),
+            nn.ReLU(),
             nn.Linear(120, 84),
-            nn.ReLU(inplace=True),
+            nn.ReLU(),
             nn.Linear(84, num_classes), # Layer 6: Output (1 120 => 10 32x32)
         )
 
@@ -51,7 +51,7 @@ class SimpleCNN(nn.Module):
         x = self.classifier(x)
         return x
 
-# Model 2 — leNet-5 with average pooling vs max pooling
+# Model 2 — LeNet-5 with Average Pooling instead of MaxPooling 
 # Reasoning: Max pooling always gets the highest value within a kernel, whilst average gets the average of all values in a kernel
 class MediumCNN(nn.Module):
 
@@ -59,19 +59,20 @@ class MediumCNN(nn.Module):
         super().__init__()
         self.features = nn.Sequential(
             nn.Conv2d(3, 6, kernel_size=5, padding=0, stride=1), # Layer 1: Convolution (1 32x32 => 6 28x28)
-            nn.ReLU(inplace=True),
-            nn.AvgPool2d(2, 2), # Layer 2: Pooling (6 28x28 => 6 14x14)
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2), # Layer 2: Pooling (6 28x28 => 6 14x14)
 
             nn.Conv2d(6, 16, kernel_size=5, padding=0), # Layer 3: Convolution (6 14x14 => 16 10x10)
-            nn.ReLU(inplace=True),
-            nn.AvgPool2d(2, 2), # Layer 4: Pooling (16 10x10 => 16 5x5)
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2), # Layer 4: Pooling (16 10x10 => 16 5x5)
             
         )
         self.classifier = nn.Sequential(
             nn.Linear(400, 120), # Layer 5: Convolution/Fully  Connected (16 5x5 => 1 120)
-            nn.ReLU(inplace=True),
+            nn.Dropout(p=0.2),
+            nn.ReLU(),
             nn.Linear(120, 84),
-            nn.ReLU(inplace=True),
+            nn.ReLU(),
             nn.Linear(84, num_classes), # Layer 6: Output (1 120 => 10 32x32)
         )
 
@@ -84,7 +85,7 @@ class MediumCNN(nn.Module):
         return x
 
 
-# Model 3 — Dropout + AvgPooling
+# Model 3 — LeNet-5 with Dropout and Averageg Pooling and a bigger kernel
 # Reasoning: Dropout is essentially dropping a % of the neurons, prevents overfitting
 class DeepCNN(nn.Module):
 
@@ -93,19 +94,19 @@ class DeepCNN(nn.Module):
         self.features = nn.Sequential(
             nn.Conv2d(3, 6, kernel_size=5, padding=0, stride=1), # Layer 1: Convolution (1 32x32 => 6 28x28)
             nn.ReLU(),
-            nn.AvgPool2d(2, 2), # Layer 2: Pooling (6 28x28 => 6 14x14)
+            nn.AvgPool2d(2,2), # Layer 2: Pooling (6 28x28 => 6 14x14)
 
             nn.Conv2d(6, 16, kernel_size=5, padding=0), # Layer 3: Convolution (6 14x14 => 16 10x10)
             nn.ReLU(),
-            nn.AvgPool2d(2, 2), # Layer 4: Pooling (16 10x10 => 16 5x5) 
+            nn.AvgPool2d(2, 2), # Layer 4: Pooling (16 14x14 => 16 10x10) 
             
         )
         self.classifier = nn.Sequential(
             nn.Linear(400, 120), # Layer 5: Convolution/Fully  Connected (16 5x5 => 1 120)
-            nn.ReLU(inplace=True),
+            nn.Dropout(p=0.2),
+            nn.ReLU(),
             nn.Linear(120, 84),
-            nn.Dropout(p=0.2, inplace=True),
-            nn.ReLU(inplace=True),
+            nn.ReLU(),
             nn.Linear(84, num_classes), # Layer 6: Output (1 120 => 10 32x32)
         )
 
